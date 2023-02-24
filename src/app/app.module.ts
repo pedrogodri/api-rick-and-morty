a88eae2c0b45faa91676f58f4b6d3066aa71b414
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-02-24 15:10:20 -0300

    Feat: criando os arquivos principais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ListCharacterComponent } from './components/lists/list-character/list-character.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    DetailsComponent,
+    ListCharacterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/lists/list-character/list-character.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-character',
+  templateUrl: './list-character.component.html',
+  styleUrls: ['./list-character.component.scss']
+})
+export class ListCharacterComponent {
+
+}
