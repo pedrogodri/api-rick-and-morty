a23adfa2223cefbeeb4b63e9d85bd3c5d2c114b8
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-02-24 15:30:07 -0300

    Alterando o APP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { DetailsComponent } from './components/details/details.component';
+import { ListCharacterComponent } from './components/lists/list-character/list-character.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path: 'list', component: ListCharacterComponent},
+  {path: 'detail/:id', component: DetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'api-rick-and-morty';
+  title = 'API Rick and Morty';
 }
