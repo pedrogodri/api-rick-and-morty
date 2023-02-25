9e9279e0c82de5c06ba5975c2ac0a2ed2099cca0
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-02-25 00:18:41 -0300

    Listando as localizações

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { ListEpisodeComponent } from './components/lists/list-episode/list-episode.component';
+
 import { ListLocationComponent } from './components/lists/list-location/list-location.component';
 import { DetailsComponent } from './components/details/details.component';
 import { ListCharacterComponent } from './components/lists/list-character/list-character.component';
@@ -11,7 +11,6 @@ const routes: Routes = [
   {path: '', redirectTo:'/home', pathMatch: 'full'},
   {path: 'listCharacter', component: ListCharacterComponent},
   {path: 'listLocation', component: ListLocationComponent},
-  {path: 'listEpisode', component: ListEpisodeComponent},
   {path: 'detail/:id', component: DetailsComponent},
 ];
 
