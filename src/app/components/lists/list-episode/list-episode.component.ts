8081721054d4dd743bf332f4285871681f5e7da9
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-02-25 00:25:23 -0300

    Listando lista de episodio (ERRADO)

--- src/app/components/lists/list-episode/list-episode.component.ts
+++ src/app/components/lists/list-episode/list-episode.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { ApiService } from './../../../service/api.service';
+import { Results } from './../../../models/api-return';
+import { Subject } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-episode',
   templateUrl: './list-episode.component.html',
   styleUrls: ['./list-episode.component.scss']
 })
-export class ListEpisodeComponent {
+export class ListEpisodeComponent implements OnInit {
+  public resultsList$ = new Subject<Results[] | undefined>();
 
+  constructor(public service: ApiService) { }
+
+  ngOnInit(): void {
+    this.service.getEpisodeList().subscribe(
+      (resp) => {
+        this.resultsList$.next(resp.results);
+      }
+    )
+  }
 }
